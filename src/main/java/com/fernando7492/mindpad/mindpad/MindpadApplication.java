c46fb88f3ba89c653673a888a463cec208d295e3
Fernando7492 <emidio8000@gmail.com>
2025-04-24 13:58:41 -0300

    project structure

---
+++ src/main/java/com/fernando7492/mindpad/mindpad/MindpadApplication.java
@@ -0,0 +1,13 @@
+package com.fernando7492.mindpad.mindpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindpadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando7492/mindpad/mindpad/MindpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando7492.mindpad.mindpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
