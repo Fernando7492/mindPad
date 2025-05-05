574a278e2377ffbbb9c4b2d121fad1df2e9d9854
Fernando7492 <emidio8000@gmail.com>
2025-05-05 20:52:38 -0300

    feat(page): page dtos created

---
+++ backend/src/main/java/com/fernando7492/mindpad/dto/PageRequestDTO.java
@@ -0,0 +1,12 @@
+package com.fernando7492.mindpad.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PageRequestDTO {
+    
+    private String title;
+    private String content;
+
+}
---
+++ backend/src/main/java/com/fernando7492/mindpad/dto/PageResponseDTO.java
@@ -0,0 +1,13 @@
+package com.fernando7492.mindpad.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter @Setter
+public class PageResponseDTO {
+
+    private Long id;
+    private String title;
+    private String content;
+}
